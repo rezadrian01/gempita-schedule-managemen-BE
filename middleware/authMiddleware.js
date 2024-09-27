b349d7938633df29be4c1ae3a14e71808d8ecde3
Fathan Alfariel <fathan.alfariel.2305356@students.um.ac.id>
2024-09-27 08:32:34 +0700

    refactor: Added role for each user and updated req.id to req.user

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,29 +3,46 @@ const Volunteer = require("../models/volunteer");
 const Student = require("../models/student");
 const Admin = require("../models/admin");
 
+// Auth Middleware for Volunteer, Student and Admin
 const authMiddleware = async (req, res, next) => {
+  // Token variable, to store the token value
   let token;
 
-  if (req?.headers?.authorization?.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
+  try {
+    // Check if authorization header exists and starts with 'Bearer'
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
 
-    try {
-      if (token) {
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
-        const id =
-          (await Volunteer.findById(decoded?.id)) ||
-          (await Student.findById(decoded?.id)) ||
-          (await Admin.findById(decoded?.id));
-
-        req.id = id;
-
-        next();
+      // Throw an error if the token is missing
+      if (!token) {
+        errResponse("No token attached to header", 401);
       }
-    } catch (err) {
-      errResponse("Not Authorized: Token expired, please login again", 401);
+
+      // Verify the token
+      const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+
+      // Check the decoded NIM in Volunteer, Student, or Admin models
+      const currentUser = await Admin.findOne({ NIM: decoded?.nim });
+      let role = "Admin";
+
+      if (!currentUser) {
+        currentUser = await Student.findOne({ NIM: decoded?.nim });
+        role = "Student";
+      }
+      if (!currentUser) {
+        currentUser = await Volunteer.findOne({ NIM: decoded?.nim });
+        role = "Volunteer";
+      }
+
+      // Attach the user information to the request object
+      req.user = { ...currentUser, role };
+
+      return next();
+    } else {
+      errResponse("No token attached to header", 401);
     }
-  } else {
-    errResponse("No token attached to header", 401);
+  } catch (err) {
+    next(err);
   }
 };
 
